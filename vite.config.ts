b274636ac7872e5d9663d0e01b9554baeaad0db6
DrBrenoMD <brenomaciel08@gmail.com>
2026-09-18 19:37:13 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, Plugin} from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import { WebSocketServer } from 'ws';
 
 function websocketPlugin(): Plugin {
@@ -35,12 +35,13 @@ function websocketPlugin(): Plugin {
           }
         });
       });
-    }
+    },
   };
 }
 
 export default defineConfig(() => {
   return {
+    base: '/Slide-Games/',
     plugins: [react(), tailwindcss(), websocketPlugin()],
     resolve: {
       alias: {
